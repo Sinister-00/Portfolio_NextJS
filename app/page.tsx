42138ec0ae2a6ecadc161345e922279cbe50f68a
Sinister-00 <sswapnil_be20@thapar.edu>
2023-09-14 10:37:08 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ export default function Page() {
   return (
     <div>
       <p className="text-sm">
-        Aspiring developer and student with a knack for frontend magic. I'm
-        well-versed in JavaScript, TypeScript, React, and Node.js, and I'm eager
-        to kickstart my career in a dynamic environment.{" "}
+        Aspiring developer and student with a knack for frontend magic. I{`'`}m
+        well-versed in JavaScript, TypeScript, React, and Node.js, and I{`'`}m
+        eager to kickstart my career in a dynamic environment.{" "}
       </p>
     </div>
   );
